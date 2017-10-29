46532d7127fc42cf5150a2e01be44e226218f089
galyamov.artur <redex2000@gmail.com>
2017-10-29 18:33:40 +0500

    refs #2856 @50m создан интеграционный тест по созданию компетенции, добавлена интернационализация

---
+++ spec/features/competences/create_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'create new competence' do
+  let(:name) { 'Веб-разработка на Rails' }
+  scenario '' do
+    visit competences_path
+    click_on 'Создать'
+    fill_in 'Название', with: name
+    fill_in 'Описание', with: 'Создание веб-приложений, используя фреймворк Rails'
+    click_on 'Создать'
+    expect(page).to have_content name
+  end
+end
