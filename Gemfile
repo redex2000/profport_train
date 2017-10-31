d5b749d6977a7f2c47863f92b201f8fa1acce340
galyamov.artur <redex2000@gmail.com>
2017-10-31 21:25:09 +0500

    refs #2857 @3h улучшен внешний вид приложения с помощью bootstrap

--- Gemfile
+++ Gemfile
@@ -30,6 +30,11 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'bootstrap-sass'
+gem 'haml'
+gem 'simple_form'
+gem 'webpacker', '~> 3.0'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def alert_class(key)
+    css_class = case key
+                  when 'notice' then
+                    'success'
+                  when 'alert' then
+                    'warning'
+                  else
+                    'danger'
+                end
+    "alert-#{css_class}"
+  end
 end
